84646a60c42b1182a0bb0fbec42b23950834ba12
Juan David Cordoba Ortega <juan941901@gmail.com>
2025-08-10 21:12:11 -0500

    actualizando repo

--- web-server/main.py
+++ web-server/main.py
@@ -1,19 +1,22 @@
+import store
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/")
+@app.get('/')
 def get_list():
-    return ["manzana","pera","Aguacate"]
+    return [1,2,3,]
 
-@app.get("/contact")
-def get_contact():
-    return {"name" : "plazit"}
-
-@app.get("/web", response_class=HTMLResponse)
-def get_web():
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
     return """
-        <h1>Hola soy una pagina web</h1>    
+        <h1>Hola soy una pagina</h1>
+        <p>soy un parrafo</p>
     """
 
+def run():
+    store.get_categories()
+
+if __name__ == '__main__':
+    run()
